b60505ad8fa18b02128cfa55ef2625c5cd9738a8
JaHyeon_Gu <jahyungu@naver.com>
2022-02-14 22:01:21 +0900

    solve 1 Lv2

---
+++ Lv2/repeat_binary_transformation.py
@@ -0,0 +1,16 @@
+def solution(s):
+    l = list(s)
+    answer = [0, 0]
+    while True:
+        if len(l) == 1:
+            break
+
+        answer[1] += l.count('0')
+        l = bin(l.count('1'))[2:]
+        answer[0] += 1
+
+    return answer
+
+print(solution("110010101001"))
+print(solution("01110"))
+print(solution("1111111"))
