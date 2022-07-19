e65f3007954c0fb49e1a5c89f0709f39f95db680
JaHyeon_Gu <jahyungu@naver.com>
2022-07-19 19:14:33 +0900

    re_solve 1 Lv3 with other method that is dfs

--- Lv3/network.py
+++ Lv3/network.py
@@ -1,4 +1,4 @@
-def solution(n, computers):
+def solution_with_recursion(n, computers):
     visited = [False] * n
     answer = 0
     for i in range(n):
@@ -13,6 +13,27 @@ def dfs(n, computers, i, visited):
     for j in range(n):
         if i != j and computers[i][j] == 1 and visited[j] == False:
             dfs(n, computers, j, visited)
+
+################################################################################
+def solution_with_stack(n, computers):
+    visited = [False] * n
+    stack = []
+    answer = n
+
+    for computer in computers:
+        for i in range(n):
+            if computer[i] == 1 and visited[i] == False:
+                stack.append(i)
+                visited[i] = True
+            while stack:
+                com = stack.pop()
+                for j in range(0, n):
+                    if visited[j] == False and computers[com][j] == 1:
+                        visited[j] = True
+                        stack.append(j)
+                        answer -= 1
+    return answer
+
 
 c = [
     [1, 0, 1, 0],
@@ -34,6 +55,6 @@ b = [
     [0, 1, 1]
 ]
 
-print(solution(3, a)) # 2
-print(solution(3, b)) # 1
-print(solution(4, c))
+print(solution_with_stack(3, a)) # 2
+print(solution_with_recursion(3, b)) # 1
+print(solution_with_stack(4, c))
